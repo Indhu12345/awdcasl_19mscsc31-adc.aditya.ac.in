4942383e151e2fe8f1458c8cbe16622654a2857e
Indhu12345 <71431379+Indhu12345@users.noreply.github.com>
2021-06-28 18:21:43 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Test
+{
+public static void main(String args[])
+{
+char ch;
+System.out.println("enter any character");
+Scanner r =new Scanner(System.in);
+ch=r.next().charAt(0);
+if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u')
+{
+System.out.println("vowel");
+}
+else
+{
+System.out.println("consonant");
+}
+}
+}
