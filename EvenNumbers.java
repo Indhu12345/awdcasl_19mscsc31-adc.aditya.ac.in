c441c9ee53ec6a033599dae9c8b5164873dcd57b
Indhu12345 <71431379+Indhu12345@users.noreply.github.com>
2021-06-28 20:26:08 +0530

    Add files via upload

--- EvenNumbers.java
+++ EvenNumbers.java
@@ -1,28 +1,24 @@
 import java.util.Scanner;
 class EvenNumbers
  {
-	private static Scanner sc;
-	public static void main(String[] args) 
-	{
-		int n,m;
-		sc = new Scanner(System.in);
-		
-		System.out.print(" Enter the n value : ");
-		n = sc.nextInt();			
-		
-		System.out.print(" Enter the m value : ");
-		m = sc.nextInt();	
-		
-		ReverseEvenNumbers(n, m);
-	}
-	public static void ReverseEvenNumbers(int n, int m)
-	{
-		int i;
-		
-		for(i = n; i >= m; i--)
-		{
+private static Scanner sc;
+public static void main(String[] args) 
+{
+int n,m;
+sc = new Scanner(System.in);
+System.out.print(" Enter the n value : ");
+n = sc.nextInt();			
+System.out.print(" Enter the m value : ");
+m = sc.nextInt();	
+ReverseEvenNumbers(n, m);
+}
+public static void ReverseEvenNumbers(int n, int m)
+{
+int i;
+for(i = n; i >= m; i--)
+{
 if(i%2==0)
-			System.out.print(i +" "); 
-		}	
-	}
+System.out.print(i +" "); 
+}	
+}
 }
